7fd4344b327f4c479acd516572a2985d7fe0e413
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2024-05-29 16:25:31 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import { IoLogoWhatsapp } from "react-icons/io";
 
 function App() {
    const [darkMode, setDarkMode] = useState(false);
@@ -20,6 +21,15 @@ function App() {
       <Projects darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+     <div className="tooltip-container">
+     <button id="">
+      <a href="https://wa.me/60828908">
+         <IoLogoWhatsapp id="fixedButton" className='mr-16 text-4xl  text-green-500 shadow-2xl'/>
+      </a>
+      </button>
+      <span className="tooltip-text">Info supplémentaire</span>
+     </div>
+      
    </div>
     )
 }
